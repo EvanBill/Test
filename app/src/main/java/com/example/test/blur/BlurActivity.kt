dbaceb203e5a220561476bd31319c66643e442c3
zhangzhanhua <zzh1028752529@gmail.com>
2025-02-09 16:07:05 +0800

    1.音频波形图-高斯模糊

---
+++ app/src/main/java/com/example/test/blur/BlurActivity.kt
@@ -0,0 +1,23 @@
+package com.example.test.blur
+
+import android.os.Bundle
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.namespace.databinding.ActivityBlurBinding
+import com.example.namespace.databinding.ActivityMainBinding
+import com.example.test.work.WorkManagerUtil
+import timber.log.Timber
+
+class BlurActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityBlurBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.btnWork.setOnClickListener {
+
+            Timber.tag("fff").e("--主Thread${Looper.getMainLooper().thread.name}")
+            WorkManagerUtil.startWork(this)
+        }
+
+    }
+}
